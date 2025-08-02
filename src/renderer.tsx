63a72b2b787a6d94faa2fb0c5750c81c63e865d9
Moinuddin Masud <moinuddinmasud@gmail.com>
2025-08-01 17:47:51 -0700

    react app running

---
+++ src/renderer.tsx
@@ -0,0 +1,12 @@
+import { createRoot } from 'react-dom/client';
+
+const App = () => {
+  return (
+    <div>
+        <h1>💖 Hello World!</h1>
+    </div>
+  );
+}
+
+const root = createRoot(document.getElementById('root'));
+root.render(<App />);
